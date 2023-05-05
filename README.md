8e7a6e1b40e6d48a64a245d51cbc53f223153e5d
Daniel Knauth <dk+git@krachbumm.de>
2023-05-05 23:09:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pass
+
+A simple [Pass](https://www.passwordstore.org) menu for MacOS
+
+People who like to keep their passwords to themselves use [Pass](https://www.passwordstore.org). However, a decent MacOS integration
+seemed to be wanting.
+
+Pass is a simple menu dropdown that copies the selected password
+to the clipboard for the configured amount of time (or 45 seconds).
+
+## Credits
+
+The app was made with [Platypus](https://sveinbjorn.org/platypus), a great way to turn some measly lines of code into a full blown Mac app.
+
+The shell script borrows from [passmenu](https://git.zx2c4.com/password-store/tree/contrib/dmenu/passmenu).
---
+++ password-store.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+PATH="/usr/local/bin:$PATH"
+shopt -s nullglob
+
+if [ $# = 0 ] ; then
+  prefix=${PASSWORD_STORE_DIR-~/.password-store}
+  prefix=$(readlink -f "$prefix")
+  
+  find $prefix -type d | sort | while read dir ; do
+    [[ "$dir" =~ .git ]] && continue
+    password_files=( "$dir"/*.gpg )
+    password_files=( "${password_files[@]#"$prefix"/}" )
+    password_files=( "${password_files[@]%.gpg}" )
+    [ "${#password_files[@]}" = 0 ] && continue
+    
+    if [[ "$dir" = "$prefix" ]] ; then
+      printf "%s\n" "${password_files[@]}"
+    else
+      printf -v password_files "|%s" "${password_files[@]}"
+      echo "SUBMENU|${dir#$prefix/}${password_files}"
+    fi
+  done
+
+else
+  
+  cmd="show"
+  what="Password"
+  
+  if pass show "$1" | fgrep -q "otpauth://" ; then
+    cmd="otp"
+    what="OTP token"
+  fi
+  
+  pass "$cmd" -c "$1" && echo "NOTIFICATION:$1|$what copied to clipboard"
+fi
